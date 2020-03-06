850afaccb8c1747b36590a5cef0e3e36ccca337d
Anthony Tristan-Padilla <aatris39>
2020-03-06 11:15:07 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 import requests
 
-URL = "http://api.open-notify.org/astros.json"
+URL = "http://www.omdbapi.com/?t=My+Hero+Academia%3A+Heroes+Rising&apikey=a7ad1aed"
+
+info = {
+    "Actors": "Kiyotaka Furushima, Tasuku Hatanaka, Ryô Hirohashi, Yoshimasa Hosoya",
+    "Director": "Kenji Nagasaki",
+    "Genre": "Animation, Action, Adventure, Comedy, Family, Sci-Fi",
+    "Language": "Japanese",
+    "Plot": "A group of youths aspiring to become professional superheroes, fight in a world full of people with abilities, also known as quirks. Deku and his fellow classmates from Hero Academy face Nine, the strongest villain yet.",
+    "Release Date": "26 Feb 2020",
+    "Runtime": "104 min",
+    "Title": "My Hero Academia: Heroes Rising"
+}
+
 
 res = requests.get(URL) # get the data
 res = res.json() # convert data to Python format
